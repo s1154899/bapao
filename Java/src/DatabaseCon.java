9bd225977e64e0e339bd42317827aedff77c56cb
s1154899 <s1154899@student.windesheim.nl>
2021-04-29 15:04:07 +0200

    netwerk_test_upload

---
+++ Java/src/DatabaseCon.java
@@ -0,0 +1,80 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+class DatabaseCon{
+
+    String userName = "app";
+    String password = "admin";
+    String dbms = "mysql";
+    String serverName;
+    String portNumber = "3306";
+
+
+    public static void main(String args[]) throws ClassNotFoundException {
+
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+        DatabaseCon dcon = new DatabaseCon("127.0.0.1");
+        try {
+            Connection connection = dcon.getConnection();
+            DatabaseCon.viewTable(connection);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public DatabaseCon(String serverName){
+        this.serverName = serverName;
+        try {
+            Connection con = getConnection();
+            DatabaseCon.viewTable(con);
+        } catch (SQLException throwables) {
+            //throwables.printStackTrace();
+        }
+    }
+
+
+    public Connection getConnection() throws SQLException {
+
+        Connection conn = null;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", this.userName);
+        connectionProps.put("password", this.password);
+
+        if (this.dbms.equals("mysql")) {
+            conn = DriverManager.getConnection(
+                    "jdbc:" + this.dbms + "://" +
+                            this.serverName +
+                            ":" + this.portNumber + "/sensors",
+                    connectionProps);
+        }
+//        else if (this.dbms.equals("derby")) {
+//            conn = DriverManager.getConnection(
+//                    "jdbc:" + this.dbms + ":" +
+//                            this.dbName +
+//                            ";create=true",
+//                    connectionProps);
+//        }
+        System.out.println("Connected to database");
+        return conn;
+    }
+
+
+    public static void viewTable(Connection con) throws SQLException {
+        String query = "SELECT * FROM `meting`";
+        try (Statement stmt = con.createStatement()) {
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String coffeeName = rs.getString("meting");
+
+                System.out.println(coffeeName);
+            }
+        } catch (SQLException e) {
+            //e.printStackTrace();
+        }
+    }
+}
