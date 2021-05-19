e8c0df87e335305a5e6c0c96529bdadc641bd5e1
s1154899 <s1154899@student.windesheim.nl>
2021-05-19 16:23:37 +0200

    songs updated

--- Java/src/Songs.java
+++ Java/src/Songs.java
@@ -22,6 +22,50 @@ public class Songs extends JDialog implements ActionListener{
         songNames.add("test 4");
         songNames.add("test 5");
 
+
+        RaspberryPi pi = new RaspberryPi("192.168.2.7");
+        String[] songs = pi.musicDirJava();
+
+        JFrame frame = new JFrame();
+
+        frame.setSize(500,500);
+
+        frame.setLayout(new BorderLayout());
+
+        JPanel test = new JPanel();
+        test.setPreferredSize(new Dimension( frame.getWidth() / 2 - 20, songs.length * 100));
+        JScrollPane scrollFrame = new JScrollPane(test);
+        test.setAutoscrolls(true);
+        scrollFrame.setPreferredSize(new Dimension( frame.getWidth() / 2,300));
+        frame.add(scrollFrame, BorderLayout.LINE_START);
+
+
+        GridLayout grid = new GridLayout(songs.length,1);
+
+        test.setLayout(grid);
+
+
+
+        for (String s : songs){
+            JButton songButton = new JButton();
+
+//            songButton.setSize(frame.getWidth() / 2, 50 );
+            songButton.setPreferredSize(new Dimension(20, 50 ));
+            songButton.setText(s);
+            songButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        pi.databaseCon.playmusic(s);
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+                }
+            });
+
+
+
+
         for (String name: songNames){
             JButton songName = new JButton(name);
             songName.addActionListener(this);
