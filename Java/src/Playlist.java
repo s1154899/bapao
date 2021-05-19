4c8e048f23947562451d78f62b0e7a6101255b27
JustRosa14 <78366746+JustRosa14@users.noreply.github.com>
2021-05-19 16:49:08 +0200

    end of day

---
+++ Java/src/Playlist.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Playlist extends JDialog{
+    ArrayList<String> songs = new ArrayList<>();
+
+
+
+    public Playlist(){
+
+
+
+
+
+
+    }
+}
