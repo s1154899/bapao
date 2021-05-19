126ec566ac385a46d3d0eccbc9387ff6bdfcda7d
s1154899 <s1154899@student.windesheim.nl>
2021-05-19 13:37:33 +0200

    music working

--- Java/src/test.java
+++ Java/src/test.java
@@ -4,7 +4,11 @@ import raspberry.SubnetCheck;
 import javax.swing.*;
 import java.awt.*;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import raspberry.RaspberryPi;
 
 
 public class test extends JDialog {
@@ -32,12 +36,54 @@ public class test extends JDialog {
     public static void main(String args[]){
 
         JFrame frame = new JFrame();
+
+        frame.setSize(500,500);
+
+        frame.setLayout(new BorderLayout());
+
+        JPanel test = new JPanel();
+        test.setPreferredSize(new Dimension( frame.getWidth() / 2,2000));
+        JScrollPane scrollFrame = new JScrollPane(test);
+        test.setAutoscrolls(true);
+        scrollFrame.setPreferredSize(new Dimension( frame.getWidth() / 2,300));
+        frame.add(scrollFrame, BorderLayout.LINE_START);
+
+
+        GridLayout grid = new GridLayout(10,1);
+
+        test.setLayout(grid);
+
+        RaspberryPi pi = new RaspberryPi("192.168.2.7");
+
+
+        for (String s : pi.musicDirJava()){
+            JButton songButton = new JButton();
+
+//            songButton.setSize(frame.getWidth() / 2, 50 );
+            songButton.setPreferredSize(new Dimension(20, 50 ));
+            songButton.setText(s);
+            songButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        pi.databaseCon.playmusic(s);
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+                }
+            });
+
+            test.add(songButton);
+        }
+
+        frame.setVisible(true);
+
 //        test t = new test(frame,true);
 
 //        t.setSize(500,500);
 //        t.setVisible(true);
 
-        SubnetCheck subnetCheck = new SubnetCheck(frame,true);
+        //SubnetCheck subnetCheck = new SubnetCheck(frame,true);
 
 
 //        try {
